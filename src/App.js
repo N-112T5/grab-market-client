371b917fafbacd25ec01431aea43706ffc3e4148
N-112T5 <woong501298@gmail.com>
2022-03-18 10:36:26 +0900

    antd 적용

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import 'antd/dist/antd.css';
 import MainPageComponent from './main/index.js';
-import {Switch, Route} from 'react-router-dom';
+import {Switch, Route, useHistory} from 'react-router-dom';
 import UploadPage from "./upload";
 import ProductPage from './product';
+import { Link } from 'react-router-dom';
+import { Button } from 'antd';
+import { DownloadOutlined } from '@ant-design/icons';
 
 function App() {
+  const history = useHistory();
   return (
     <div>
         <div id="header">
           <div id="header-area">
-            <img src="/images/icons/logo.png"/>
+            <Link to="/">
+              <img src="/images/icons/logo.png"/>
+            </Link>
+            <Button size="large" onClick={function(){
+              history.push('/upload');
+            }} icon={<DownloadOutlined />}>
+              Upload product
+            </Button>
           </div>
         </div>
       <div id="body">
